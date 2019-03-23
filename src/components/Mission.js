976198645853d233c6bcccddfa67b6732a6e8707
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-03-23 13:10:07 -0700

    finalizing location

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -13,11 +13,12 @@ export default class Mission extends Component {
                     </div>
                     <div className="mission-statement">
                         <h2>Vision Statement</h2>
-                        <p>A church that transforms homes into Christ-centered families<br/>예수 그리스도의 은혜로 가정이 변화되는 가족 공동체 교회</p>
+                        <p>A church that transforms homes into Christ-centered families</p>
+                        <p id="korean">예수 그리스도의 은혜로 가정이 변화되는 가족 공동체 교회</p>
                     </div>
                 </div>
                 <div className="mission-community-container" style={{marginTop: '1em', width: '85%'}}>
-                    <div className="mission-statement">
+                    <div id="korean" className="mission-statement">
                         <p>하나님의 은혜와 영광을 경험하는<br/><h2>예배 공동체</h2></p>
                         <p>복음안에서 삶을 나누고 인생이 성숙해지는<br/><h2>나눔 공동체</h2></p>
                         <p>약속의 말씀에 기쁨으로 순종하는<br/><h2>제자 공동체</h2></p>
