efaa115972db7e5c25877c757ff1df4a1cba84f9
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-03-25 21:51:22 -0700

    hover affect on about carousel compeleted

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ export default class About extends Component {
         return(
             <div>
             <div className="about-header">
-                <p className="header-title"><span><i class="fas fa-quote-left"></i></span>한 사람</p>
-                <p className="header-pop">한 영혼</p>
+                <p className="header-title"><span><i class="fas fa-quote-left" style={{fontSize: '40px', padding: '0 20px 35px 0'}}></i></span>한 사람</p>
+                <p className="header-pop">한 영혼<span><i class="fas fa-quote-right" style={{fontSize: '40px', paddingLeft: '20px', position: 'absolute'}}></i></span></p>
             </div>
                 {/* <div className="test">
                 <h3>한 영혼</h3>
