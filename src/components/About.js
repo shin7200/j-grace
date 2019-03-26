04ad6514c6eb5e91b6571d480211ad66accceee5
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-03-25 22:20:01 -0700

    commit before pull

--- src/components/About.js
+++ src/components/About.js
@@ -4,15 +4,12 @@ import '../styles/about.css';
 export default class About extends Component {
     render() {
         return(
-            <div>
+
             <div className="about-header">
                 <p className="header-title"><span><i class="fas fa-quote-left" style={{fontSize: '40px', padding: '0 20px 35px 0'}}></i></span>한 사람</p>
                 <p className="header-pop">한 영혼<span><i class="fas fa-quote-right" style={{fontSize: '40px', paddingLeft: '20px', position: 'absolute'}}></i></span></p>
             </div>
-                {/* <div className="test">
-                <h3>한 영혼</h3>
-                </div> */}
-            </div>
+
         )
     }
 }
