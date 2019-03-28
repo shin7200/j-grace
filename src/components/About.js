d18c682448c93517ce842706047b7f579e34f2e8
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-03-28 11:47:29 -0700

    added hoverspace on about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,9 @@ export default class About extends Component {
                 <div className="about-header" data-aos="fade-down" data-aos-duration="1500">
                     <p className="header-title"><span><i class="fas fa-quote-left" style={{fontSize: '40px', padding: '0 20px 35px 0'}}></i></span>한 사람</p><br/>
                     <p className="header-pop">한 영혼<span><i class="fas fa-quote-right" style={{fontSize: '40px', paddingLeft: '20px', position: 'absolute'}}></i></span></p>
+                    <div className="hover-space"></div>
                 </div>
+                
                 <div className="about-mission-statement-container">
                     <div className="about-mission-statement-welcome">
                         <p>Welcome!</p>
