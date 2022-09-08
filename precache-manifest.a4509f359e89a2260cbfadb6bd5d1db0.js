ce7c0eb2c7cd7148ec23bf6a493ad80997cdf59f
shin <shin72005941@gmail.com>
2022-09-07 22:45:47 -0700

    Updates

---
+++ precache-manifest.a4509f359e89a2260cbfadb6bd5d1db0.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1708482d4a0f4eeba84",
+    "url": "/static/css/main.41594f14.chunk.css"
+  },
+  {
+    "revision": "e1708482d4a0f4eeba84",
+    "url": "/static/js/main.041bdb6b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bece1c69b341b6af0f49",
+    "url": "/static/css/2.aac0af30.chunk.css"
+  },
+  {
+    "revision": "bece1c69b341b6af0f49",
+    "url": "/static/js/2.ee65867b.chunk.js"
+  },
+  {
+    "revision": "eca7614c8433284fdb0b2bd1d4193fb9",
+    "url": "/static/media/logo.eca7614c.png"
+  },
+  {
+    "revision": "0a8b82ab7b8d408682fc39127bf0eb03",
+    "url": "/static/media/footerLogo.0a8b82ab.jpeg"
+  },
+  {
+    "revision": "d216f8eafdb3a21c629ae0d99f57c0bc",
+    "url": "/static/media/mission.d216f8ea.png"
+  },
+  {
+    "revision": "429486b4c4465d9b57866a883743e07b",
+    "url": "/static/media/pastorFamily2.429486b4.jpeg"
+  },
+  {
+    "revision": "71e7dda6ab0daff03b7c941c0844b011",
+    "url": "/static/media/lastWeeklyUpdate.71e7dda6.png"
+  },
+  {
+    "revision": "96284b46b5719a864bbf97ec3872ceb2",
+    "url": "/static/media/carouselOne.96284b46.jpg"
+  },
+  {
+    "revision": "9646d2b0beeddaf875ad98c68b28e8c2",
+    "url": "/static/media/carouselTwo.9646d2b0.png"
+  },
+  {
+    "revision": "f7b56b86553a987b2903e6476d7ded19",
+    "url": "/static/media/carouselThree.f7b56b86.png"
+  },
+  {
+    "revision": "05e39dc98feba79b1846beed3633603b",
+    "url": "/static/media/carouselFour.05e39dc9.png"
+  },
+  {
+    "revision": "682f2eb79ef9dab308d63b6abde45680",
+    "url": "/static/media/2020Mission.682f2eb7.pdf"
+  },
+  {
+    "revision": "59f4e2b534382725acb2864baac5d6e7",
+    "url": "/static/media/service.59f4e2b5.png"
+  },
+  {
+    "revision": "4f246eacdfb032a1bb4bc5852f27ad2b",
+    "url": "/static/media/thisMonth.4f246eac.png"
+  },
+  {
+    "revision": "3e425fd21f75ffa63ee40fd01748098a",
+    "url": "/static/media/about.3e425fd2.jpeg"
+  },
+  {
+    "revision": "d182b6759dd436fff6e6fbfbef201f7e",
+    "url": "/static/media/location.d182b675.png"
+  },
+  {
+    "revision": "720f3bcc2e159294be7b311ed57af587",
+    "url": "/static/media/news.720f3bcc.jpeg"
+  },
+  {
+    "revision": "2b3e017f50013632fd53ebd3aaa9d937",
+    "url": "/static/media/sermon.2b3e017f.jpg"
+  },
+  {
+    "revision": "3163a84f562cfca121badb6930cc0a7a",
+    "url": "/static/media/covid19.3163a84f.jpg"
+  },
+  {
+    "revision": "0222b877ed36a1a238982c7e70f0114d",
+    "url": "/static/media/EMPS.0222b877.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "8ac5a65d14bbfe802e36aabb8e530cb9",
+    "url": "/index.html"
+  }
+];
